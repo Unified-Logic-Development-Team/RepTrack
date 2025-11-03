4e5eb8d0cd746d6e2aac9e3cdb76f616346c0b2e
Matt <mjcantin@yahoo.com>
2025-11-03 14:56:16 -0700

    Add main class, no functionality yet

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("RepTrack");
+        System.out.println("1. Track Workout\n" +
+                           "2. View History");
+    }
+}
